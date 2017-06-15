ed787b8e135cf7b8f905292b8e87d1a600863538
Taylor Fausak <taylor@fausak.me>
2017-06-15 07:40:13 -0500

    Track link clicks

--- content/templates/base.js
+++ content/templates/base.js
@@ -1,3 +1,14 @@
 window.ga = function () { ga.q.push(arguments) }
 ga.l = +new Date
 ga.q = [['create', 'UA-99854195-1', 'auto'], ['send', 'pageview']]
+
+document.addEventListener('click', function (e) {
+  if (e.target.tagName.toLowerCase() === 'a') {
+    e.preventDefault()
+    url = e.target.href
+    ga('send', 'event', 'link', 'click', url, {
+      transport: 'beacon',
+      hitCallback: function () { document.location = url }
+    })
+  }
+})
